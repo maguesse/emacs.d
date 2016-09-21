6bc73fb67896417bdf64b8c6ce58e1adb93d315a
maguesse <maguesse@gmail.com>
2016-09-21 23:17:30 +0200

    Added theme support

--- settings/setup-appareance.el
+++ settings/setup-appareance.el
@@ -34,4 +34,14 @@
       display-time-format "%a, %d %b %Y %H:%M")
 (display-time-mode 1)
 
+;; Theme
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+
+;; TODO : create a my/load-theme to be customized by the user?
+;; For example the "ohai-personal-taste/style" we can see in Bodil's ohai-apparence.el
+;; https://github.com/bodil/ohai-emacs/blob/master/modules/ohai-appearance.el#L101
+(use-package solarized-theme
+  :config
+  (load-theme 'solarized-dark t))
+
 (provide 'setup-appareance)
